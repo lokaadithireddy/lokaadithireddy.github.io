089b2acdd6ee5ee307c5bbbb3a8709b0031ca828
karlok <lokaadithireddy22@gmail.com>
2026-06-17 23:04:19 -0700

    maint1

--- js/script.js
+++ js/script.js
@@ -1,41 +1,51 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(window).on('load', function() {
+    $(window).on('load', function () {
         $('.spinner').fadeOut(500);
     });
 
-    //Smooth transition
-    $(function() {
-        $('a.page-scroll').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-            }, 1500, 'swing');
-            event.preventDefault();
-        });
+    // Fallback in case the load event fires before the handler attaches.
+    setTimeout(function () {
+        $('.spinner').fadeOut(500);
+    }, 1200);
+
+    // Smooth transition with fixed-navbar offset.
+    $('a.page-scroll').on('click', function (event) {
+        var $anchor = $(this);
+        var target = $($anchor.attr('href'));
+        if (!target.length) {
+            return;
+        }
+
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - 70
+        }, 800, 'swing');
+        event.preventDefault();
     });
 
-    //scroll spy
+    // Scroll spy.
     $('body').scrollspy({
-        target: '.navbar-default'
-    })
+        target: '.navbar-default',
+        offset: 90
+    });
 
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
     });
 
-        
-        $('.jav').click(function () {
-            $('#jav-modal').modal('show');
-        });
-        $('.web').click(function () {
-            $('#web-modal').modal('show');
-        });
-        $('.ml').click(function () {
-            $('#ml-modal').modal('show');
-        });
-        $('.bigdata').click(function () {
-            $('#bigdata-modal').modal('show');
-        });
+    function openModal(selector) {
+        $(selector).modal('show');
+    }
 
+    $('.jav').on('click', function () { openModal('#jav-modal'); });
+    $('.web').on('click', function () { openModal('#web-modal'); });
+    $('.ml').on('click', function () { openModal('#ml-modal'); });
+    $('.bigdata').on('click', function () { openModal('#bigdata-modal'); });
+
+    $('.picture-container').on('keydown', function (event) {
+        if (event.key === 'Enter' || event.key === ' ') {
+            event.preventDefault();
+            $(this).trigger('click');
+        }
+    });
 });
